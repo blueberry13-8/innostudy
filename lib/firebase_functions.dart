5439f620f013a12f284c1f806285776fea9e952f
Blueberry13 <gee8way@gmail.com>
2022-06-13 18:08:20 +0300

    ADD addFolderInGroup() and deleteFolderFromGroup() EDIT addGroup() and deleteGroup()

--- lib/firebase_functions.dart
+++ lib/firebase_functions.dart
@@ -1,10 +1,11 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'folder.dart';
 import 'group.dart';
 
 /// Add group to the DB if it doesn't exist.
 void addGroup(Group group) {
-  final data = {"groupName": group.groupName};
+  final data = group.toJson();
   var database = FirebaseFirestore.instance;
   database.collection('groups').doc(group.groupName).get().then((value) {
     if (value.exists) {
@@ -34,6 +35,31 @@ void deleteGroup(Group group) {
 /// Use it for dynamic rendering Group List.
 final Stream<QuerySnapshot> groupsStream =
     FirebaseFirestore.instance.collection('groups').snapshots();
+
+void addFolderInGroup(Group group, Folder folder){
+  var database = FirebaseFirestore.instance;
+  var data = folder.toJson();
+  database.collection('groups').doc(group.groupName).collection('folders').doc(folder.folderName).get().then((value) {
+    if (value.exists) {
+      debugPrint('');
+    } else {
+      database.collection('groups').doc(group.groupName).collection('folders').doc(folder.folderName).set(data);
+      debugPrint('');
+    }
+  });
+}
+
+void deleteFolderFromGroup(Group group, Folder folder){
+  var database = FirebaseFirestore.instance;
+  database.collection('groups').doc(group.groupName).collection('folders').doc(folder.folderName).get().then((value) {
+    if (value.exists) {
+      database.collection('groups').doc(group.groupName).collection('folders').doc(folder.folderName).delete();
+      debugPrint('');
+    } else {
+      debugPrint('');
+    }
+  });
+}
 
 /// Add file to the selected group (if group exists).
 void addFileToGroup(Group group, String filePath) {
