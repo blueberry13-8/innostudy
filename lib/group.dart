8e8a5e02badc7b4950711ff267bebb6808751a63
Blueberry13 <gee8way@gmail.com>
2022-06-13 18:07:04 +0300

    ADD toJson() and fromJson()

--- lib/group.dart
+++ lib/group.dart
@@ -9,4 +9,18 @@ class Group {
   List<Folder> folders;
 
   Group({required this.groupName, required this.folders});
+
+  factory Group.fromJson(Map<String, dynamic> json) {
+    return Group(
+      groupName: json['groupName'],
+      folders: json['folders'],
+    );
+  }
+
+  Map<String, dynamic> toJson() { // 2
+    return {
+      'groupName': groupName,
+      'folders': folders,
+    };
+  }
 }
