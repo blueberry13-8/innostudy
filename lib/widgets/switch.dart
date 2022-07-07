57356caa48bc95233ae4a7565393fd9d17610d57
vladdan16 <v.danshov@innopolis.university>
2022-07-07 13:57:27 +0300

    Fixed switch

--- lib/widgets/switch.dart
+++ lib/widgets/switch.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class FolderTypeSwitch extends StatefulWidget {
   const FolderTypeSwitch({Key? key, required this.callback}) : super(key: key);
@@ -16,16 +17,29 @@ class _FolderTypeSwitchState extends State<FolderTypeSwitch> {
   Widget build(BuildContext context) {
     return Row(
       children: [
-        const Text('With Files'),
+        Text(
+          'With Files',
+          style: TextStyle(
+            color: Theme.of(context).primaryColor,
+            fontSize: 17,
+          ),
+        ),
         CupertinoSwitch(
-            value: withFolder,
-            onChanged: (value) {
-              withFolder = !withFolder;
-              //debugPrint(withFolder.toString());
-              setState(() {});
-              widget.callback(withFolder);
-            }),
-        const Text('With Folders'),
+          trackColor: Theme.of(context).focusColor,
+          value: withFolder,
+          onChanged: (value) {
+            withFolder = !withFolder;
+            setState(() {});
+            widget.callback(withFolder);
+          },
+        ),
+        Text(
+          'With Folders',
+          style: TextStyle(
+            color: Theme.of(context).primaryColor,
+            fontSize: 17,
+          ),
+        ),
       ],
     );
   }
